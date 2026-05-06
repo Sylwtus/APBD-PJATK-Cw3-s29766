a5a75abe3242d2e1967c5148fde08b1c21d5e092
Dawid Fiuk <fiukdavid@gmail.com>
2026-05-06 22:42:18 +0200

    Adding Reservations model

---
+++ Models/Reservation.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TrainingCenterApi.Models;
+
+public class Reservation
+{
+    public int Id { get; set; }
+
+    [Required]
+    public int RoomId { get; set; }
+
+    [Required]
+    public string OrganizerName { get; set; }
+
+    [Required]
+    public string Topic { get; set; }
+
+    [Required]
+    public DateTime Date { get; set; }
+
+    [Required]
+    public TimeSpan StartTime { get; set; }
+
+    [Required]
+    public TimeSpan EndTime { get; set; }
+
+    [Required]
+    public string Status { get; set; }
+}
